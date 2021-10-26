b7b0d0848d4f7e6de9d58b75713ecc6cfac9396d
huythaislna <46748354+huythaislna@users.noreply.github.com>
2021-10-26 08:10:03 +0700

    Create Solution.py

---
+++ S/Solution.py
@@ -0,0 +1,17 @@
+class Order:
+	def addItem(self):
+    # add Items
+    pass
+
+	def removeItem(self):
+    # remove Items
+    pass
+  
+class Payment:
+		def pay(self, type):
+		if self.type == 'debit':
+				# do something 
+        pass
+		if self.type == 'credit':
+	      # ....
+        pass
