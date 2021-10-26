5db49b265348aae475b496da8a3c286fe169b810
huythaislna <46748354+huythaislna@users.noreply.github.com>
2021-10-26 08:27:49 +0700

    Create Violation.py

---
+++ I/Violation.py
@@ -0,0 +1,16 @@
+# Violation of SRP
+class Phone:
+	def call()
+	def wifi()
+	def use3g():
+
+class ModernPhone(Phone):
+  pass
+
+class Telephone(Phone):
+	pass
+
+class S40Phone(Phone):
+  pass
+
+
